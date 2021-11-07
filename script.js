f57a4bdac13b16367695a243ad5127706bda31d7
Andrey <ajelobenko@mail.ru>
2021-11-07 14:35:29 +0300

    Урок 2.7. Исправление заполнения объекта expenses

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let money = prompt('Ваш бюджет на месяц?', ''),
 	time = prompt('Введите дату в формате YYYY - MM - DD', '');
 
@@ -11,11 +13,11 @@ let appData = {
 };
 
 let a1 = prompt('Введите обязательную статью расходов в этом месяце', ''),
-	a2 = prompt('Во сколько обойдется', '');
-a3 = prompt('Введите обязательную статью расходов в этом месяце', ''),
+	a2 = prompt('Во сколько обойдется', ''),
+	a3 = prompt('Введите обязательную статью расходов в этом месяце', ''),
 	a4 = prompt('Во сколько обойдется', '');
 
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
+appData.expenses[a1] = a2;
+appData.expenses[a3] = a4;
 
 alert(appData.budget / 30);
